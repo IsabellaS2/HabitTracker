8abe42d32167def43d3ec1fc094362df37d836d5
Isabella Sulisufi <114592322+IsabellaSulisufi@users.noreply.github.com>
2026-05-01 16:24:13 +0100

    add basic design and fucntionality

--- HabitTracker/HabitModel.swift
+++ HabitTracker/HabitModel.swift
@@ -5,13 +5,50 @@
 //  Created by Isabella Sulisufi on 16/04/2026.
 //
 
-struct HabitModel {
-    let habitName: String
-    let habitIcon: Icon
-    let habitGoal: String
-    let habitNotes: String
+import SwiftUI
+
+struct HabitModel: Identifiable {
+    var id = UUID()
+    var habitName: String
+    var habitIcon: Icon
+    var isCompleted: Bool = false
+
+//    let habitGoal: String
+//    let habitNotes: String
 }
 
-enum Icon {
+enum Icon: CaseIterable, Identifiable {
     case water, movement, read, eat, meditate
+
+    var id: Self { self }
+
+    var systemImage: String {
+        switch self {
+        case .water: return "drop.fill"
+        case .movement: return "figure.walk"
+        case .read: return "book.fill"
+        case .eat: return "fork.knife"
+        case .meditate: return "brain.head.profile"
+        }
+    }
+
+    var title: String {
+        switch self {
+        case .water: return "Hydration"
+        case .movement: return "Movement"
+        case .read: return "Reading"
+        case .eat: return "Nutrition"
+        case .meditate: return "Mindfulness"
+        }
+    }
+
+    var color: Color {
+        switch self {
+        case .water: return .blue
+        case .movement: return .green
+        case .read: return .orange
+        case .eat: return .red
+        case .meditate: return .purple
+        }
+    }
 }
